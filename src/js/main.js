909c0f6e1adfb9dda6652aa88a726a86181b78d7
Vimal KR <vimalkr141@gmail.com>
2022-08-02 19:50:49 +0530

    sticky

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,26 @@
-const nav = document.querySelector('.stickynav');
-const header = document.querySelector('header');
+const nav = document.getElementById('nav_bar')
+const header = document.getElementById('header');
+const form = document.getElementById('form_data');
+const left_nav = document.getElementById('nav_left');
+const right_nav = document.getElementById('nav_right');
+const mainbody = document.getElementById('main_body');
+
 
 const options = {}
 
 const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            right_nav.classList.remove('nav_right ')
+        }
+        else {
+            right_nav.classList.add('nav_right ')
+            console.log('interscet')
+        }
+    })
+}, options)
+
+const newObserver = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             nav.classList.remove('changecolour')
@@ -15,4 +32,5 @@ const observer = new IntersectionObserver(entries => {
     })
 }, options)
 
-observer.observe(header)
+newObserver.observe(header)
+observer.observe(form)
